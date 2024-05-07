9e546d1aee4213f94b02b9614c1ff4216d90ce41
J-Oskam <justinoskam@gmail.com>
2024-05-07 14:59:21 -0400

    everything from the 2 hour point. added migrations, category index view and category controller

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers {
+    public class CategoryController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
